8d81d07915dbe39bb7b4fa34e0f44a16a496fa4c
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-07 22:41:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,6 @@ function addElementToBeginningOfArray(array, element) {
   //return array with added element to begginning without changing array}
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var newArray = []
   //use .unshift (modifies original content)
   return //add element in a way that alters original array
   }
