c06089fbcf95809022c724b11c10a42fad735f8b
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-07 22:15:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,55 +1,55 @@
 //define variable
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(array, elementtoadd) {
-  var thisArray = []
+
+function addElementToBeginningOfArray(arr, elementtoadd) {
+  var array = ['foo', 'bar'];
+  ['addthiselementtobeginning', ...arr];
   //use spread operator to add
-  return //return array with added element to begginning without changing array
+  addElementToBeginningOfArray();//return array with added element to begginning without changing array
 }
 
-function destructivelyAddElementToBeginningOfArray(array, elementtoadd) {
-  var thisArray = []
+function destructivelyAddElementToBeginningOfArray(arr, elementtoadd) {
+  var arr = []
   //use .unshift (modifies original content)
   return //add element in a way that alters original array
   }
 
-  function addElementToEndOfArray(array, elementtoadd) {
-    var thisArray = []
-    //add element to the end without altering original
-    return
+  function addElementToEndOfArray(arr, elementtoadd) {
+    var arr = []
+    [...arr, 'addthiselementtobeginning']//add element to the end without altering original
   }
 
-  function destructivelyAddElementToEndOfArray(array, elementtoadd) {
-    var thisArray = []
+  function destructivelyAddElementToEndOfArray(arr, elementtoadd) {
+    var arr = []
     //add element to alter original .push
     return
   }
 
-  function accessElementInArray(array, index) {
-    var thisArray = []
+  function accessElementInArray(arr, index) {
+    var arr = []
     return //element in that index - lookup index definition
   }
 
-  function destructivelyRemoveElementFromBeginningOfArray(array) { //array is the only argument
-    var thisArray = []
+  function destructivelyRemoveElementFromBeginningOfArray(arr) { //array is the only argument
+    var arr = []
     //remove first value destructively
     return //the array with first value removed. use shift
   }
 
-  function removeElementFromBeginningOfArray(array) {
-    var thisArray = []
+  function removeElementFromBeginningOfArray(arr) {
+    var arr = []
     //remove element from beginning of array non-destructive
       //instructions say return ENTIRE array ???
     return
   }
 
-  function destructivelyRemoveElementFromEndofArray(array) {
-    var thisArray = []
+  function destructivelyRemoveElementFromEndofArray(arr) {
+    var arr = []
     //remove last element destructively (pop)
     return //returns mutated Array - again says ENTIRE???
   }
 
-  function removeElementFromEndOfArray(array) {
-    var thisArray = []
+  function removeElementFromEndOfArray(arr) {
+    var arr = []
     return //array without the last element - meaning we will have to assign it - //use slice then assign 
   }
