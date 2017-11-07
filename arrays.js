0e2428ddfe256e1f1af329bfba5847ccb0eb744d
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-07 22:49:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
+
   }
   //use spread operator to add
   //return array with added element to begginning without changing array}
