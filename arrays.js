449017d9f16f92d216d525400984817f6e7ab944
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-07 22:00:52 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,57 @@
+//define variable
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, elementtoadd) {
+  var thisArray = []
+  //use spread operator to add 
+  return //return array with added element to begginning without changing array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, elementtoadd) {
+  var thisArray = []
+  //use .unshift (modifies original content)
+  return //add element in a way that alters original array
+  }
+
+  function addElementToEndOfArray(array, elementtoadd) {
+    var thisArray = []
+    //add element to the end without altering original
+    return 
+  }
+
+  function destructivelyAddElementToEndOfArray(array, elementtoadd) {
+    var thisArray = []
+    //add element to alter original .push
+    return
+  }
+
+  function accessElementInArray(array, index) {
+    var thisArray = []
+    return //element in that index - lookup index definition
+  }
+
+  function destructivelyRemoveElementFromBeginningOfArray(array) { //array is the only argument
+    var thisArray = []
+    //remove first value destructively 
+    return //the array with first value removed. use shift
+  }
+
+  function removeElementFromBeginningOfArray(array) {
+    var thisArray = []
+    //remove element from beginning of array non-destructive
+      //instructions say return ENTIRE array ???
+    return 
+  }
+
+  function destructivelyRemoveElementFromEndofArray(array) {
+    var thisArray = []
+    //remove last element destructively (pop)
+    return //returns mutated Array - again says ENTIRE???
+  }
+
+  function removeElementFromEndOfArray(array) {
+    var thisArray = []
+    return //array without the last element - meaning we will have to assign it - //use slice then assign 
+  }
+
+  
