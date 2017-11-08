24a1f07737e18f261c977a95a8cdb5b04676b81d
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-08 00:52:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,45 +1,44 @@
 // //define variable
 // var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-// 
+//
 // function addElementToBeginningOfArray(array, element) {
 //   return [element, ...array]
 //   }
 //   //use spread operator to add
 //   //return array with added element to begginning without changing array}
-// 
+//
 // function destructivelyAddElementToBeginningOfArray(array, element) {
 //   array.unshift(element)
 //   return array
 //   }
-// 
+//
 //   function addElementToEndOfArray(array, element) {
 //     return [...array, element]
 //   }
-// 
+//
 //   function destructivelyAddElementToEndOfArray(array, element) {
 //     array.push(element)
 //     return array
 //   }
-// 
+//
 //   function accessElementInArray(array, index) {
 //     return array[index]
 //   //element in that index - lookup index definition
 //   }
-// 
+//
 //   function destructivelyRemoveElementFromBeginningOfArray(array) { //array is the only argument
 //     array.shift(1)
 //     return array
 //   }
-// 
+//
 //   function removeElementFromBeginningOfArray(array) {
 //     return array.slice(1)
 //   }
 
   function destructivelyRemoveElementFromEndofArray(array) {
-    array.pop()
+    array.pop(-1)
     return array
-    //remove last element destructively (pop)
-    //returns mutated Array - again says ENTIRE???
+  
   }
 
   function removeElementFromEndOfArray(array) {
