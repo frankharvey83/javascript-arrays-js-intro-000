e31edd1c32806ebecb75d0cba9a54f2730da1801
Frank H Mcmanus <frank@thefrankharvey.com>
2017-11-08 00:45:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   }
 
   function destructivelyRemoveElementFromBeginningOfArray(array) { //array is the only argument
-    return array.shift()
+    return array.shift(1)
   }
 
   function removeElementFromBeginningOfArray(array) {
